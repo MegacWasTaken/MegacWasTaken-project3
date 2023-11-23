6dc85b4702f76477e27a4741a0f89f18050d56bd
jsherwood00 <142844002+jsherwood00@users.noreply.github.com>
2023-11-23 16:23:55 -0500

    Reorganized: now the stage can be accessed from the Controller class

--- gui/java_files/HomeScreenController.java
+++ gui/java_files/HomeScreenController.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class HomeScreenController {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeScreenController {
+    private Stage stage;
+
+    public void setStage(Stage stage){
+        this.stage = stage;
+    }
+
+    private void findButtonClicked() { //TODO: proportions
+        // here, instead of the previous stuff, we just set up the stage to
+        // be using a new fxml file that we make with Scene Builder for the "find" screen
+        try{
+            Parent findRoot = FXMLLoader.load(getClass().getResource("/FindGUI.fxml"));
+            stage.getScene().setRoot(findRoot);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void newButtonClicked(){
+
+    }
 }
--- gui/java_files/newGUI.java
+++ gui/java_files/newGUI.java
@@ -1,33 +1,29 @@
-package java_files;
-
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollBar;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.event.EventHandler;
-
 
 
 public class newGUI extends Application{
 
+    private static Stage primaryStage;
+
+    public static Stage getPrimaryStage(){
+        return primaryStage;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("/newGUI.fxml"));
+        newGUI.primaryStage = primaryStage;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/HomeScreenGUI.fxml"));
+        Parent root = loader.load();
+
+        HomeScreenController controller = loader.getController();
+        controller.setStage(primaryStage);
+
         primaryStage.setTitle("Code Lib");
         primaryStage.setScene(new Scene(root, 1080, 600));
         primaryStage.show();
