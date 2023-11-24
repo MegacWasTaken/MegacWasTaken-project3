d34f0dcd8d3fbed1c3be275bf85c93a8d48e884e
MegacWasTaken <megacsoldier@gmail.com>
2023-11-24 00:28:55 -0500

    Find functioning

--- src/main/java/gui/HomeScreenController.java
+++ src/main/java/gui/HomeScreenController.java
@@ -1,31 +1,44 @@
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class HomeScreenController {
     private Stage stage;
 
-    public void setStage(Stage stage){
+    public void setStage(Stage stage) {
         this.stage = stage;
     }
 
-    public void findButtonClicked() { //TODO: proportions
+    public void findButtonClicked() { // TODO: proportions
         // here, instead of the previous stuff, we just set up the stage to
-        // be using a new fxml file that we make with Scene Builder for the "find" screen
-        try{
-            Parent findRoot = FXMLLoader.load(getClass().getResource("/gui/FindGUI.fxml"));
+        // be using a new fxml file that we make with Scene Builder for the "find"
+        // screen
+        try {
+            // Create the path with current dir + specific
+            String basePath = System.getProperty("user.dir");
+            String filePath = basePath + "/src/main/resources/gui/FindGUI.fxml";
+
+            // Create file with path, take URL from this file
+            File fxmlFile = new File(filePath);
+            URL fxmlLocation = fxmlFile.toURI().toURL();
+
+            // Create an FXMLLoader from this location, set new root as result of .load() on
+            // this
+            FXMLLoader loader = new FXMLLoader(fxmlLocation);
+            Parent findRoot = loader.load();
+
+            // Update scene with this new element as root
             stage.getScene().setRoot(findRoot);
-        } catch(IOException e){
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void newButtonClicked(){
+    public void newButtonClicked() {
 
     }
 }
